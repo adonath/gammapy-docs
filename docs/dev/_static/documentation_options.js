6f0a47778ad0dc5214d7a5f77a3bb3ea48c32053
Christoph Deil <Deil.Christoph@gmail.com>
2018-04-17 08:06:24 +0200

    update docs/dev

--- docs/dev/_static/documentation_options.js
+++ docs/dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.8.dev5774',
+    VERSION: '0.8.dev5797',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
