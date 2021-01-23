1158f0ae2354d23a7845a4b94c8bb0d9af89cbca
Jose Enrique Ruiz <jer@iaa.es>
2021-01-23 11:13:12 +0100

    update dev docs

--- docs/dev/_static/documentation_options.js
+++ docs/dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.3.dev361+ge1744c61b',
+    VERSION: '0.18.3.dev429+g6f9fa30bd.d20210123',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
